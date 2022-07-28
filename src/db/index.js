1ee6e7ce0269b867ec6f30f0c8335e0fa27607fe
Henrique Kramer <henriquemkramer@gmail.com>
2022-07-28 16:13:35 -0300

    first commit

---
+++ src/db/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  usuarios: [
+    { 
+      id: 1,
+      nome: "Henrique",
+      email: "henrique@email.com",
+      telefone: "41 1234 5678",
+      perfil: 1
+  
+    },
+    { 
+      id: 2,
+      nome: "Delbia",
+      email: "delbia@email.com",
+      telefone: "41 4321 8765",
+      perfil: 2
+    }
+  ],
+  perfis: [
+    { id: 1, descricao: "ADMIN"},
+    { id: 2, descricao: "NORMAL"},
+  ]
+}

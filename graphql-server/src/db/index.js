20fc1d55db98071f99b789733afe8e69dd747ff6
Henrique Kramer <henriquemkramer@gmail.com>
2022-08-01 14:45:12 -0300

    Ajuste db

--- graphql-server/src/db/index.js
+++ graphql-server/src/db/index.js
@@ -2,30 +2,30 @@ module.exports = {
   contatos: [
     { 
       id: 1,
-      nome: "Henrique Kramer",
-      email: "henrique@email.com",
+      nome: "Contato 1",
+      email: "contato1@email.com",
       telefone: "41 1234 5678",
       perfil_id: 1
   
     },
     { 
       id: 2,
-      nome: "Delbia Furquim",
-      email: "delbia@email.com",
+      nome: "Contato 2",
+      email: "contato2@email.com",
       telefone: "41 4321 8765",
       perfil_id: 2
     },
     { 
       id: 3,
-      nome: "Vera Lucia",
-      email: "vera@email.com",
+      nome: "Contato 3",
+      email: "contato3@email.com",
       telefone: "41 4657 1745",
       perfil_id: 2
     },
     { 
       id: 4,
-      nome: "Carlos Alberto",
-      email: "carlos@email.com",
+      nome: "Contato 4",
+      email: "contato4@email.com",
       telefone: "41 3578 8765",
       perfil_id: 2
     }
